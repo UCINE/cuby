504a309d40af8df54b9ed50ba87afb2298be4f48
Omar Jamal <ojamal@student.1337.ma>
2023-10-21 20:59:03 +0100

    deleted obj files

--- obj/check_colors.d
+++
@@ -1,3 +0,0 @@
-obj/check_colors.o: src/check_colors.c src/cub3d.h src/../libft/libft.h \
-  src/../minilibx_opengl_20191021/mlx.h \
-  src/../get_next_line/get_next_line.h
--- obj/map_check.d
+++
@@ -1,3 +0,0 @@
-obj/map_check.o: src/map_check.c src/cub3d.h src/../libft/libft.h \
-  src/../minilibx_opengl_20191021/mlx.h \
-  src/../get_next_line/get_next_line.h
