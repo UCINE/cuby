eb605c4b3e8c42912d79b9fedfe69b30a7cf8c3c
Lahoucine Hamouni <lahamoun@e2r13p9.1337.ma>
2023-10-21 09:09:49 +0100

    update

---
+++ obj/map_check.d
@@ -0,0 +1,3 @@
+obj/map_check.o: src/map_check.c src/cub3d.h src/../libft/libft.h \
+  src/../minilibx_opengl_20191021/mlx.h \
+  src/../get_next_line/get_next_line.h
---
+++ obj/map_size.d
@@ -0,0 +1,3 @@
+obj/map_size.o: src/map_size.c src/cub3d.h src/../libft/libft.h \
+  src/../minilibx_opengl_20191021/mlx.h \
+  src/../get_next_line/get_next_line.h
